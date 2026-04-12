e951437c0576c8452cab93a8842edc3ff5386d36
Kenji Saito <ken-yo@mbr.nifty.com>
2026-04-12 14:22:27 +0900

    Bumps node modules

---
+++ update.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+CUR=$(pwd)
+
+CURRENT=$(cd "$(dirname "$0")" || exit;pwd)
+echo "${CURRENT}"
+
+cd "${CURRENT}" || exit
+git pull --prune
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CURRENT}" || exit
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+echo ""
+pwd
+npx -y pnpm@latest self-update && pnpm install -r && rm -rf pnpm-lock.yaml && pnpm up -r && pnpm audit --fix && pnpm up -r && pnpm -r --if-present lint-fix && pnpm build
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CURRENT}" || exit
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+git pull --prune && git commit -am "Bumps node modules" && git push
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CUR}" || exit
